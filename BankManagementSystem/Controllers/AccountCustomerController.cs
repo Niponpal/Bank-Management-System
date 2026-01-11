e5cd750b4b5229a0e0cf5dcf8a660c9521c1a007
Niponpal <niponpal52@gmail.com>
2026-01-11 13:05:10 +0600

    Fix AccountCustomer Controller 🚀🚀🚑

---
+++ BankManagementSystem/Controllers/AccountCustomerController.cs
@@ -0,0 +1,19 @@
+﻿using BankManagementSystem.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem.Controllers;
+
+public class AccountCustomerController : Controller
+{
+    private readonly IAccountCustomerRepository _accountCustomerRepository;
+    public AccountCustomerController(IAccountCustomerRepository accountCustomerRepository)
+    {
+        _accountCustomerRepository = accountCustomerRepository;
+    }
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var data = await _accountCustomerRepository.GetAllAccountCustomerAsync(cancellationToken);
+        return View(data);
+    }
+}
