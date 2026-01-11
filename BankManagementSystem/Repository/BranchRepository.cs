46ba921b6cf9e71116c30d2e6371aabe56a2e7c1
Niponpal <niponpal52@gmail.com>
2026-01-11 21:06:57 +0600

    Fix IbranchRepository 🚀🚀🚑

---
+++ BankManagementSystem/Repository/BranchRepository.cs
@@ -0,0 +1,37 @@
+﻿using BankManagementSystem.Data;
+using BankManagementSystem.Models;
+
+namespace BankManagementSystem.Repository;
+
+public class BranchRepository : IBranchRepository
+{
+    private readonly ApplicationDbContext _context;
+    public BranchRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public Task<Branch> AddBranchAsync(Branch branch, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Branch> DeleteBranchAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Branch>> GetAllBranchAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Branch?> GetBranchByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Branch?> UpdateBranchAsync(Branch branch, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BankManagementSystem/Repository/IBranchRepository.cs
@@ -0,0 +1,12 @@
+﻿using BankManagementSystem.Models;
+
+namespace BankManagementSystem.Repository;
+
+public interface IBranchRepository
+{
+    Task<IEnumerable<Branch>> GetAllBranchAsync(CancellationToken cancellationToken);
+    Task<Branch?> GetBranchByIdAsync(int id, CancellationToken cancellationToken);
+    Task<Branch> AddBranchAsync(Branch  branch, CancellationToken cancellationToken);
+    Task<Branch?> UpdateBranchAsync(Branch  branch, CancellationToken cancellationToken);
+    Task<Branch> DeleteBranchAsync(int id, CancellationToken cancellationToken);
+}
